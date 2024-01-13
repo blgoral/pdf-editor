64e47d96e57e376f33905e94de0f771264ab9336
Blake Goral <blakegoral@gmail.com>
2024-01-13 14:08:00 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { pdf } from "pdfjs-dist";
+import { fabric } from "fabric";
+
+function component() {
+    const element = document.createElement('div');
+  
+    
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
